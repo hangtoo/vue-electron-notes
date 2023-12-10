035530393ca8e33113e4bf5b0d6b6c3d40da7bc0
hlf <qn_lf@163.com>
2023-12-10 17:39:40 +0800

    feat: m1

--- src/background.js
+++ src/background.js
@@ -20,7 +20,8 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      enableRemoteModule: true
+      enableRemoteModule: true,
+      contextIsolation: false
     }
   })
 
